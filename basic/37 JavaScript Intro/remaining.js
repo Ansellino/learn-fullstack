5967f6b9b43e0ccfd74a78d0a1221ae32bc78601
AnselZorro <ansellino@gmail.com>
2025-01-11 20:06:52 +0700

    learn slice

--- basic/37 JavaScript Intro/remaining.js
+++ basic/37 JavaScript Intro/remaining.js
@@ -1,4 +1,5 @@
 var tweet = prompt("compose your tweet:");
 var tweetCount = tweet.length()
+var tweetUnderlined = tweet.slice(0,148);
 alert("You have written" + tweetCount + "Characters, you have" +(255 - tweetCount) + "characters remaining.");
-
+alert(tweetUnderlined);
