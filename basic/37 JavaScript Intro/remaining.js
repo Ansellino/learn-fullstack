519fdc114d41239efecb1d497005a21ada13a4f7
AnselZorro <ansellino@gmail.com>
2025-01-11 20:02:59 +0700

    remaining tweet

---
+++ basic/37 JavaScript Intro/remaining.js
@@ -0,0 +1,4 @@
+var tweet = prompt("compose your tweet:");
+var tweetCount = tweet.length()
+alert("You have written" + tweetCount + "Characters, you have" +(255 - tweetCount) + "characters remaining.");
+
