86c67e01c34f9cc524965f220ea7319fadecf6d1
AnselZorro <ansellino@gmail.com>
2025-01-11 20:48:54 +0700

    learn js

---
+++ basic/37 JavaScript Intro/leftYear.js
@@ -0,0 +1,19 @@
+function lifeInWeeks(age) {
+    
+    /************Don't change the code above************/    
+        
+        //Write your code here.
+        var leftYear = 90 - age;
+        var days = leftYear  * 365;
+        var weeks =  leftYear * 52;
+        var months = leftYear * 12;
+        
+        console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left." );
+        
+        
+        
+        
+    /*************Don't change the code below**********/
+    }
+    
+    
