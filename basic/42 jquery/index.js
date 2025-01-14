213b72bc00a4f6a1cc09e884a4825b6f2cd7a85e
AnselZorro <ansellino@gmail.com>
2025-01-14 21:11:01 +0700

    make simon game

---
+++ basic/42 jquery/index.js
@@ -0,0 +1,36 @@
+$("h1").on("mouseover", function(){
+    $("h1").css("color","purple");
+});
+
+/*
+before()
+after()
+prepend()
+append()
+
+for (var i = 0; i<5 ; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+        document.querySelector("h1").style.color = "purple";
+    });
+}
+
+$("input").keypress(function(event){
+    $("h1").text(event.key);
+});
+
+$("button").click(function() {
+    $("h1").css("color", "purple");
+});
+
+$("h1").addClass("big-title margin-50");
+$("h1").removeClass("big-title");
+$("h1").css("font-size","5rem");
+$("h1").css("color","blue"); 
+$("h1").text("Bye");
+$("button").text("Don't Click Me");
+$("a").attr("href","https://www.yahoo.com");
+
+$("h1").click(function(){
+    $("h1").css("color", "purple");
+});
+*/
